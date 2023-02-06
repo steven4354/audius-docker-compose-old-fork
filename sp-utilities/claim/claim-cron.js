d9a1d7a1e331b3c63de094c4aef48d3d364653b4
steven4354 <steven4354@gmail.com>
2023-02-06 21:39:00 +0000

    every hour

--- sp-utilities/claim/claim-cron.js
+++ sp-utilities/claim/claim-cron.js
@@ -5,7 +5,7 @@ const SP_OWNER_WALLET = "0x61A1BC089f87F1C0e38A34207D65077484d89088";
 const PK = "0x4e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e6e";
 
 const job = new CronJob(
-	'* * * * * *',
+	'0 * * * *',
 	async function() {
 		console.log('You will see this message every second');
 
@@ -25,4 +25,5 @@ const job = new CronJob(
 );
 
 // cd sp-utilities; nvm use 16; npm install
-// nvm use 16, node claim-cron.js
+// nvm use 16; node claim-cron.js
+// screen -ls to see all the screens
